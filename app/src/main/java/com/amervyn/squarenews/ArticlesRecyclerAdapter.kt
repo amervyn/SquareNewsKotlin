a965274283e02f836930d6b4d1dad0524cc1b7a9
Anto <amervyn14@gmail.com>
2018-08-21 21:29:13 +0100

    basic recycler with clickable links and spinner country selection

--- app/src/main/java/com/amervyn/squarenews/ArticlesRecyclerAdapter.kt
+++ app/src/main/java/com/amervyn/squarenews/ArticlesRecyclerAdapter.kt
@@ -1,17 +1,29 @@
 package com.amervyn.squarenews
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+import android.net.Uri
 
 
 class ArticlesRecyclerAdapter(context: Context, aresult: ArticleResult) : RecyclerView.Adapter<ArticlesRecyclerAdapter.ViewHolder>() {
 
     private var aResult : ArticleResult? = null
     private var mContext : Context? = null
+    private lateinit var clickListener : View.OnClickListener
 
     init{
             aResult=aresult
@@ -31,19 +43,88 @@ class ArticlesRecyclerAdapter(context: Context, aresult: ArticleResult) : Recycl
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val articleItem= aResult?.Articles!![position]
+        holder.headlineTextView.text = articleItem.Headline
+        //this.DownloadImageTask(holder.imageView).execute(articleItem.ImageUrl)
+
+
+        holder.itemView.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(articleItem.Url))
+            mContext?.startActivity(browserIntent)
+        }
+
+        Picasso.with(mContext)
+                .load(articleItem.ImageUrl)
+                .into(holder.imageView, object : Callback {
+                    override fun onSuccess() {
+                        holder.pBar.visibility = View.GONE
+                    }
+
+                    override fun onError() {
+                        Log.d("pBar", "Error")
+                        val errorUrl="https://dummyimage.com/600x400/ffffff/ff0000&text=Image+Unavailable"
+
+                        Picasso.with(mContext)
+                                .load(errorUrl)
+                                .into(holder.imageView)
+
+                    }
+                })
 
     }
 
     inner class ViewHolder// We also create a constructor that accepts the entire item row
     // and does the view lookups to find each subview
-    (itemView: View) : RecyclerView.ViewHolder(itemView) {
+    (itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        init{
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            Log.d("RecyclerView", "CLICK")
+        }
         // Your holder should contain a member variable
         // for any view that will be set as you render a row
 
-        var nameTextView: ImageView = itemView.findViewById(R.id.article_image) as ImageView
+        var headlineTextView: TextView = itemView.findViewById(R.id.article_headline) as TextView
 
+        var imageView: ImageView = itemView.findViewById(R.id.article_image) as ImageView
+
+        var pBar : ProgressBar = itemView.findViewById(R.id.image_progress) as ProgressBar
         // to access the context from any ViewHolder instance.
+
     }
+
+
+    internal inner class DownloadImageTask(var bmImage: ImageView) : AsyncTask<String, Void, Bitmap>() {
+
+        override fun onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute()
+            //pd.show()
+        }
+
+        override fun doInBackground(vararg urls: String): Bitmap? {
+            val urldisplay = urls[0]
+            var mIcon11: Bitmap? = null
+            try {
+                val `in` = java.net.URL(urldisplay).openStream()
+                mIcon11 = BitmapFactory.decodeStream(`in`)
+            } catch (e: Exception) {
+                Log.e("Error", e.message)
+                e.printStackTrace()
+            }
+
+            return mIcon11
+        }
+
+        override fun onPostExecute(result: Bitmap) {
+            super.onPostExecute(result)
+            //pd.dismiss()
+            bmImage.setImageBitmap(result)
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/amervyn/squarenews/MainActivity.kt
+++ app/src/main/java/com/amervyn/squarenews/MainActivity.kt
@@ -3,7 +3,6 @@ package com.amervyn.squarenews
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
-import android.widget.Toast
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import okhttp3.*
@@ -11,82 +10,127 @@ import org.json.JSONObject
 import java.io.IOException
 import android.R.attr.duration
 import android.R.attr.layout
+import android.support.v4.view.MenuItemCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import java.time.LocalDateTime
 import com.google.gson.JsonElement
 import com.google.gson.JsonParser
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.Menu
 import android.view.View
-import android.widget.LinearLayout
+import android.widget.*
+import android.widget.Spinner
 
 
-class ArticleResult(val TotalResults : Int, val Articles : List<ArticleItem>) {
+
+
+class ArticleResult(val TotalResults: Int, val Articles: List<ArticleItem>) {
     override fun toString(): String = TotalResults.toString()
 
 }
 
-class ArticleItem(val ArticleId : Int, val SourceId: Int, val NewsApiSourceId: String,
-                       val Headline : String, val Description : String, val ImageUrl : String,
-                       val Url : String, val IsAvailable : Boolean, val CreatedOn : String,
-                       val PublishedOn : String, val Country : String, val ViewCount : Int) {
+class ArticleItem(val ArticleId: Int, val SourceId: Int, val NewsApiSourceId: String,
+                  val Headline: String, val Description: String, val ImageUrl: String,
+                  val Url: String, val IsAvailable: Boolean, val CreatedOn: String,
+                  val PublishedOn: String, val Country: String, val ViewCount: Int) {
     override fun toString(): String = Headline
 }
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
-    val url="http://amervyn.duckdns.org/api/Articles"
+    var url = "http://amervyn.duckdns.org/api/Articles/?country=GB&pageSize=50"
     private var aResult: ArticleResult? = null
+    var oldCountryParam:String = "country=GB"
+
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val item= parent?.getItemAtPosition(position)
+
+        url=url.replace(oldCountryParam, "country=" + item.toString())
+
+        oldCountryParam="country=" + item.toString()
+
+        Log.d("ItemSelected", "New URL: $url")
+
+        callApi()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        //toolbar.setLogo(R.drawable.ic_fishnews_home)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        //supportActionBar?.setDisplayShowHomeEnabled(true)
+        //supportActionBar?.setDisplayUseLogoEnabled(true)
+
         //callApi()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //return super.onCreateOptionsMenu(menu)
+
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        val spinner = findViewById<Spinner>(R.id.country_spinner)
+        spinner.onItemSelectedListener = this
+// Create an ArrayAdapter using the string array and a default spinner layout
+        val adapter = ArrayAdapter.createFromResource(this,
+                R.array.countries_array, android.R.layout.simple_spinner_item)
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+// Apply the adapter to the spinner
+        spinner.adapter = adapter
+
+        return true
     }
 
 
     override fun onResume() {
         super.onResume()
-        val toast=Toast.makeText(applicationContext, "calling api...", Toast.LENGTH_SHORT)
+        val toast = Toast.makeText(applicationContext, "calling api...", Toast.LENGTH_SHORT)
         toast.show()
         callApi()
-
-        /*val recyclerAdapter=ArticlesRecyclerAdapter(this, this!!.aResult!!)
-        val recyclerView=findViewById<View>(R.id.rvArticles) as RecyclerView
-
-        recyclerView.adapter=recyclerAdapter
-        recyclerView.layoutManager=LinearLayoutManager(this)*/
     }
 
 
     private fun callApi() {
 
-        println("calling api")
+        println("loading articles...")
 
         //val proxyTest = Proxy(Proxy.Type.HTTP, InetSocketAddress("192.168.51.99", 3128))
 
         //val builder= OkHttpClient.Builder().proxy(proxyTest)
-        val client=OkHttpClient()
+        val client = OkHttpClient()
 
         val request = Request.Builder()
                 .url(url)
                 .build()
 
         // Get a handler that can be used to post to the main thread
-        client.newCall(request).enqueue(object: Callback {
+        client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 e.printStackTrace()
                 runOnUiThread {
-                    val toast=Toast.makeText(applicationContext, e.message, Toast.LENGTH_LONG)
+                    val toast = Toast.makeText(applicationContext, e.message, Toast.LENGTH_LONG)
                     toast.show()
                 }
             }
+
             @Throws(IOException::class)
             override fun onResponse(call: Call, response: Response) {
 
-                val responseData= response.body()?.string()
+                val responseData = response.body()?.string()
                 println(responseData)
 
                 /*val json = JSONObject(responseData)
@@ -96,28 +140,31 @@ class MainActivity : AppCompatActivity() {
 
                 val parser = JsonParser()
                 val mJson = parser.parse(responseData)
-                val gson=GsonBuilder().create()
-                val articleFeed=gson.fromJson(mJson, ArticleResult::class.java)
+                val gson = GsonBuilder().create()
+                val articleFeed = gson.fromJson(mJson, ArticleResult::class.java)
 
-                if (!response.isSuccessful)
-                {
+                if (!response.isSuccessful) {
                     runOnUiThread {
-                        val toast=Toast.makeText(applicationContext, "api call failed", Toast.LENGTH_SHORT)
+                        val toast = Toast.makeText(applicationContext, "api call failed", Toast.LENGTH_SHORT)
                         toast.show()
                     }
                     throw IOException("Unexpected code $response")
-                }
-                else
-                {
-                    if(articleFeed!=null)
-                    {
-                        Log.d("Debug", "Total Results: " + articleFeed.TotalResults.toString())
+                } else {
+                    if (articleFeed != null) {
+                        Log.d("Debug", "Finished loading: " + articleFeed.TotalResults.toString() + " articles")
 
                         runOnUiThread {
-                            val toast=Toast.makeText(applicationContext, articleFeed.TotalResults.toString(), Toast.LENGTH_SHORT)
+                            val toast = Toast.makeText(applicationContext, "Finished loading: " + articleFeed.TotalResults.toString() + " articles", Toast.LENGTH_SHORT)
                             toast.show()
 
-                            aResult=articleFeed
+                            aResult = articleFeed
+
+                            val recyclerAdapter = ArticlesRecyclerAdapter(applicationContext, articleFeed)
+                            val recyclerView = findViewById<View>(R.id.rvArticles) as RecyclerView
+
+                            recyclerView.adapter = recyclerAdapter
+                            recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+
                         }
                     }
 
